fd56dea9b5a2aeb30896574e23ce77febba4475a
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-15 07:19:55 +0500

    TypesOfMob

--- src/main/java/com/mr_toad/lib/api/entity/type/records/Add2HorseTypesRecord.java
+++ src/main/java/com/mr_toad/lib/api/entity/type/records/Add2HorseTypesRecord.java
@@ -2,4 +2,5 @@ package com.mr_toad.lib.api.entity.type.records;
 
 import net.minecraft.resources.ResourceLocation;
 
-public record Add2HorseTypesRecord(int id, ResourceLocation name, ResourceLocation textureLocation) {}
+public record Add2HorseTypesRecord(int id, ResourceLocation name, ResourceLocation textureLocation) {
+}
