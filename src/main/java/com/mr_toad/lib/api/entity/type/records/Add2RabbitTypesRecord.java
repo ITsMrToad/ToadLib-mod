731ce3585db6a98b9f3a9a95e0e474c0660dc4ff
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-15 07:20:35 +0500

    TypesOfMob

--- src/main/java/com/mr_toad/lib/api/entity/type/records/Add2RabbitTypesRecord.java
+++ src/main/java/com/mr_toad/lib/api/entity/type/records/Add2RabbitTypesRecord.java
@@ -2,4 +2,5 @@ package com.mr_toad.lib.api.entity.type.records;
 
 import net.minecraft.resources.ResourceLocation;
 
-public record Add2RabbitTypesRecord(int id, ResourceLocation name, ResourceLocation textureLocation) {}
+public record Add2RabbitTypesRecord(int id, ResourceLocation name, ResourceLocation textureLocation) {
+}
