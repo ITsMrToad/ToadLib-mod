5cad6793a7c27b5cd610cdde6dff3cee3cbcc4e1
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-15 07:23:41 +0500

    bla-bla

--- src/main/java/com/mr_toad/lib/api/IFreezer.java
+++ src/main/java/com/mr_toad/lib/api/IFreezer.java
@@ -5,9 +5,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.text.html.parser.Entity;
 
 public interface IFreezer<T extends Entity> {
-
     @Nullable T getIceCube();
-
     void setIceCube(@Nullable T target);
-
 }
