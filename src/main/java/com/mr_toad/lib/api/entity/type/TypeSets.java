6fff1957634ff413231a6557c288352d80685398
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-15 07:22:44 +0500

    Inital commit

--- src/main/java/com/mr_toad/lib/api/entity/type/TypeSets.java
+++ src/main/java/com/mr_toad/lib/api/entity/type/TypeSets.java
@@ -8,9 +8,7 @@ import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import java.util.Set;
 
 public class TypeSets {
-
     public static final Set<Add2HorseTypesRecord> HORSE_TYPES = new ObjectArraySet<>();
     public static final Set<Add2RabbitTypesRecord> RABBIT_TYPES = new ObjectArraySet<>();
     public static final Set<Add2ParrotTypesRecord> PARROT_TYPES = new ObjectArraySet<>();
-
 }
