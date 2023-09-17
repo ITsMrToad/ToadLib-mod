18312084a9eae80c54579772685dcf487c15a127
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-17 16:32:25 +0500

    Create IPlayer || 1.0.2 version update

---
+++ src/main/java/com/mr_toad/lib/api/IRobber.java
@@ -0,0 +1,9 @@
+package com.mr_toad.lib.api;
+
+import net.minecraft.world.entity.LivingEntity;
+
+public interface IRobber<T extends LivingEntity>{
+    T getRobber();
+  
+  T setRobber(T robber);
+}
