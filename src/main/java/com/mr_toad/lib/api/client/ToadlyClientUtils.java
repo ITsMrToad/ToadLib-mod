faf6503a4c5c1af2eef2b6996127f85e0f2c1de6
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-19 22:36:32 +0500

    Refactor ToadlyClientUtils || 1.0.3 version update

--- src/main/java/com/mr_toad/lib/api/client/ToadlyClientUtils.java
+++ src/main/java/com/mr_toad/lib/api/client/ToadlyClientUtils.java
@@ -4,12 +4,12 @@ import net.minecraft.resources.ResourceLocation;
 
 public class ToadlyClientUtils {
 
-  public static ModelLayerLocation register(String name) {
-        return register(name, "main");
+  public static ModelLayerLocation main(String modid, String name) {
+        return register(modid, name, "main");
     }
 
-  public static ModelLayerLocation register(String name, String layer) {
-        return new ModelLayerLocation(new ResourceLocation(VillageUpgrade.MODID, name), layer);
+  public static ModelLayerLocation register(String modid, String name, String layer) {
+        return new ModelLayerLocation(new ResourceLocation(modid, name), layer);
     }
 
 
