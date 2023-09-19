ed0e63c51dacb5f0a06db4afa3f726be5dbafd99
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-19 22:09:14 +0500

    Create ICrossEntityContainer || 1.0.3 version update

--- src/main/java/com/mr_toad/lib/api/blockentity/ICrossEntityContainer.java
+++ src/main/java/com/mr_toad/lib/api/blockentity/ICrossEntityContainer.java
@@ -4,11 +4,6 @@ import net.minecraft.world.Container;
 import net.minecraft.world.entity.LivingEntity;
 
 public interface CrossEntityContainer extends Container {
-
     default void startEntityOpen(LivingEntity entity) {}
-
     default void stopEntityOpen(LivingEntity entity) {}
-
-
-
 }
