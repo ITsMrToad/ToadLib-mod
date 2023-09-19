53ca2870b178c19df795047b26e23467eabcf730
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-19 22:16:10 +0500

    Remove private | Add static || 1.0.3 version update

--- src/main/java/com/mr_toad/lib/api/util/ToadOuterUtils.java
+++ src/main/java/com/mr_toad/lib/api/util/ToadOuterUtils.java
@@ -12,48 +12,45 @@ import java.util.Objects;
 
 public class ToadDataUtils {
 
-    private String itemName(Item item) {
+    public static String itemName(Item item) {
         return Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)).getPath();
     }
 
-    private String itemName(Block block) {
+    public static String itemName(Block block) {
         return Objects.requireNonNull(ForgeRegistries.BLOCKS.getKey(block)).getPath();
     }
 
-    public ResourceLocation resourceBlock(String path, String modid) {
+    public static ResourceLocation resourceBlock(String path, String modid) {
         return new ResourceLocation(modid, "block/" + path);
     }
 
-    public ResourceLocation resourceItem(String path, String modid) {
+    public static ResourceLocation resourceItem(String path, String modid) {
         return new ResourceLocation(modid, "item/" + path);
     }
 
-    public String advName(String name, String modid) {
-        return modid + ":" + name;
-    }
 
     public static RegistryObject<SoundEvent> registerSounds(String name, String modid, DeferredRegister<SoundEvent> soundEventDeferredRegister) {
         return soundEventDeferredRegister.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(modid, name)));
     }
 
-    public void blockBasedModel(Item item, String suffix) {
+    public static void blockBasedModel(Item item, String suffix) {
         withExistingParent(itemName(item), resourceBlock(itemName(item) + suffix));
     }
 
 
-    public void itemGeneratedModel(Item item, ResourceLocation texture) {
+    public static void itemGeneratedModel(Item item, ResourceLocation texture) {
         withExistingParent(itemName(item), "item/generated").texture("layer0", texture);
     }
 
-    private String itemName(Item item) {
+    public static String itemName(Item item) {
         return Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)).getPath();
     }
 
-    public ResourceLocation resourceBlock(String path) {
+    public static ResourceLocation resourceBlock(String path) {
         return new ResourceLocation(VillageUpgrade.MODID, "block/" + path);
     }
 
-    public ResourceLocation resourceItem(String path) {
+    public static ResourceLocation resourceItem(String path) {
         return new ResourceLocation(VillageUpgrade.MODID, "item/" + path);
     }
 }
