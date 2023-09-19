0f974747096f7aa7ba4d1b3ea0ae6802e6c04b7b
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-19 21:58:12 +0500

    Add new methods | Rename || 1.0.3 version update

--- src/main/java/com/mr_toad/lib/api/util/ToadOuterUtils.java
+++ src/main/java/com/mr_toad/lib/api/util/ToadOuterUtils.java
@@ -35,4 +35,25 @@ public class ToadDataUtils {
     public static RegistryObject<SoundEvent> registerSounds(String name, String modid, DeferredRegister<SoundEvent> soundEventDeferredRegister) {
         return soundEventDeferredRegister.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(modid, name)));
     }
+
+    public void blockBasedModel(Item item, String suffix) {
+        withExistingParent(itemName(item), resourceBlock(itemName(item) + suffix));
+    }
+
+
+    public void itemGeneratedModel(Item item, ResourceLocation texture) {
+        withExistingParent(itemName(item), "item/generated").texture("layer0", texture);
+    }
+
+    private String itemName(Item item) {
+        return Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)).getPath();
+    }
+
+    public ResourceLocation resourceBlock(String path) {
+        return new ResourceLocation(VillageUpgrade.MODID, "block/" + path);
+    }
+
+    public ResourceLocation resourceItem(String path) {
+        return new ResourceLocation(VillageUpgrade.MODID, "item/" + path);
+    }
 }
