6ab83ca082c296e60f24243215681f80195de982
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-24 15:04:59 +0500

    Moved || 1.0.4 update

---
+++ src/main/java/com/mr_toad/lib/api/item/tab/FilledBlockItem.java
@@ -0,0 +1,20 @@
+package com.mr_toad.lib.api.item.enums.tab;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.block.Block;
+
+public class FilledBlockItem extends BlockItem implements ICreativeTabFiller{
+
+    public FilledBlockItem(Block block, Properties p) {
+        super(block, p);
+    }
+
+    @Override
+    public void fill(CreativeModeTab.Output output) {
+        output.accept(this);
+    }
+
+
+
+}
