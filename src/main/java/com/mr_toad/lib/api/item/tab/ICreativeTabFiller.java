047b5baa36d87dc316664e05ac9506192ec3223b
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-24 15:04:41 +0500

    .

--- src/main/java/com/mr_toad/lib/api/item/tab/ICreativeTabFiller.java
+++
@@ -1,7 +0,0 @@
-package com.mr_toad.lib.api.item.enums.tab;
-
-import net.minecraft.world.item.CreativeModeTab;
-
-public interface ICreativeTabFiller {
-    void fill(CreativeModeTab.Output output);
-}
