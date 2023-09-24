033afe94b307923aa3db759cc2a51e1a0e01c8fd
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-24 15:02:35 +0500

    New ICreativeTabFiller | Create '/item' || 1.0.4 update

---
+++ src/main/java/com/mr_toad/lib/api/item/enums/tab/FilledItem.java
@@ -0,0 +1,18 @@
+package com.mr_toad.lib.api.item.enums.tab;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+
+public class FilledItem extends Item implements ICreativeTabFiller {
+
+    public FilledItem(Properties p) {
+        super(p);
+    }
+
+    @Override
+    public void fill(CreativeModeTab.Output output) {
+        output.accept(this);
+    }
+
+
+}
