89e83d563ed05caf6de7154ee6d16603916a2df7
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-24 15:04:11 +0500

    Add files via upload
    moved || 1.0.4 update

---
+++ src/main/java/com/mr_toad/lib/api/item/tab/ICreativeTabFiller.java
@@ -0,0 +1,7 @@
+package com.mr_toad.lib.api.item.enums.tab;
+
+import net.minecraft.world.item.CreativeModeTab;
+
+public interface ICreativeTabFiller {
+    void fill(CreativeModeTab.Output output);
+}
