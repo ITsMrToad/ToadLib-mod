894eea332ef487a41055e8aa897fa20084aed636
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-24 15:40:52 +0500

    Delete IRobber || 1.0.4 update

--- src/main/java/com/mr_toad/lib/api/IRobber.java
+++
@@ -1,9 +0,0 @@
-package com.mr_toad.lib.api;
-
-import net.minecraft.world.entity.LivingEntity;
-
-public interface IRobber<T extends LivingEntity>{
-    T getRobber();
-  
-    T setRobber(T robber);
-}
