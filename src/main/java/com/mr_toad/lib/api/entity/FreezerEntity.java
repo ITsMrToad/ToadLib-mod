d84fc9935b38005d70c5c09d8e675859ab85322c
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2023-09-30 21:44:12 +0500

    Rename IFreezer || 1.0.5 update

--- src/main/java/com/mr_toad/lib/api/entity/FreezerEntity.java
+++ src/main/java/com/mr_toad/lib/api/entity/FreezerEntity.java
@@ -3,7 +3,7 @@ package com.mr_toad.lib.api.entity;
 import net.minecraft.world.entity.Entity;
 import org.jetbrains.annotations.Nullable;
 
-public interface IFreezer<T extends Entity> {
+public interface FreezerEntity<T extends Entity> {
     @Nullable T getIceCube();
 
     void setIceCube(@Nullable T var1);
