c978a1100c8d0b0742aadaa9dbe19d67b93e2dac
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-01-18 21:46:16 +0500

    Create IntegerCooldown || 1.0.6 update

---
+++ src/main/java/com/mr_toad/lib/api/util/time/IntegerCooldown.java
@@ -0,0 +1,88 @@
+package com.mr_toad.lib.api.util.time;
+
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public class IntegerCooldown {
+
+    public int cooldown;
+    @Nullable public final String name;
+    public boolean immutable = false;
+    public static final Logger LOGGER = LoggerFactory.getLogger("Cooldown");
+
+    public static final IntegerCooldown BASIC = IntegerCooldown.createNoName(500);
+
+    public static IntegerCooldown createNoName(int cooldown) {
+        return new IntegerCooldown(cooldown, null);
+    }
+
+    public IntegerCooldown(int cooldown, @Nullable String name) {
+        this.cooldown = cooldown;
+        if (name == null || name.isEmpty()) {
+            name = this.toString();
+        }
+        this.name = name;
+
+    }
+
+    public void reset() {
+        this.setCooldown(this.getCooldown());
+    }
+
+    public void tickUp() {
+        ++this.cooldown;
+    }
+
+    public void tickDown() {
+        --this.cooldown;
+    }
+
+    public IntegerCooldown minus(int i) {
+        this.check();
+        this.cooldown -= i;
+        return this;
+    }
+
+    public IntegerCooldown sum(int i) {
+        this.check();
+        this.cooldown += i;
+        return this;
+    }
+
+    public IntegerCooldown setImmutable() {
+        this.immutable = true;
+        return this;
+    }
+
+    public void check() {
+        if (this.immutable) {
+            throw new IllegalArgumentException("Cannot modify cooldown value with name:" + this.getName() + "because cooldown field is immutable");
+        }
+
+        if (this.getCooldown() < -1) {
+            this.setCooldown(0);
+            LOGGER.error("Cooldown value with name: {} cannot be lower than -1", this.getName());
+        }
+
+    }
+
+    public @Nullable String getName() {
+        return this.name;
+    }
+
+    public int getCooldown() {
+        return this.cooldown;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    @Override
+    public String toString() {
+        return "<int cooldown value: name = " + this.getName() + ", cooldown = " + this.getCooldown() + ">";
+    }
+}
