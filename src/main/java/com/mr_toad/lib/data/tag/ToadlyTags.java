131cdffc157a48aa08ef0607f9486d5cf9cb1a59
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-01-18 16:56:22 +0500

    Update ToadlyTags || 1.0.6 update

--- src/main/java/com/mr_toad/lib/data/tag/ToadlyTags.java
+++ src/main/java/com/mr_toad/lib/data/tag/ToadlyTags.java
@@ -4,14 +4,14 @@ import com.mr_toad.lib.api.util.ToadTagUtils;
 import com.mr_toad.lib.core.ToadLib;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Zoglin;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 
-public class ToadlyTags {
+public final class ToadlyTags {
 
-    public static class ToadlyItemTags {
-
+    public static final class ToadlyItemTags {
         public static TagKey<Item> WIRIOS = itemTag("wirios");
         public static TagKey<Item> ORES = itemTag("ores");
         public static TagKey<Item> ENDER_FOOD = itemTag("ender_food");
@@ -21,11 +21,9 @@ public class ToadlyTags {
         }
     }
 
-    public static class ToadlyBiomeTags {
+    public static final class ToadlyBiomeTags {
 
-        public static TagKey<Biome> IS_STORMWOOD_FOREST = biomeTag("is_stormwood_forest");
         public static TagKey<Biome> IS_OUTER_END = biomeTag("is_outer_end");
-
         public static TagKey<Biome> IS_PLAINSLIKE = biomeTag("is_plainslike");
 
         private static TagKey<Biome> biomeTag(String name) {
@@ -33,21 +31,22 @@ public class ToadlyTags {
         }
     }
 
-    public static class ToadlyBlockTags {
+    public static final class ToadlyBlockTags {
 
-        public static TagKey<Block> ORES = blockTag();
-
+        public static final TagKey<Block> ORES = blockTag();
         private static TagKey<Block> blockTag() {
             return ToadTagUtils.createBlock(ToadLib.MODID, "ores");
         }
     }
+    
+    public static final class ToadlyEntityTypeTags {
 
-    public static class ToadlyEntityTypeTags {
+        public static final TagKey<EntityType<?>> ZOMBIES = entityTag("zombies");
 
-        public static TagKey<EntityType<?>> ORES = entityTag();
+        public static final TagKey<EntityType<?>> NETHER_MOBS = entityTag("nether_mobs");
+        public static final TagKey<EntityType<?>> END_MOBS = entityTag("end_mobs");
 
-        private static TagKey<EntityType<?>> entityTag() {
-            return ToadTagUtils.createEntityType(ToadLib.MODID, "ores");
+        private static TagKey<EntityType<?>> entityTag(String name) {
+            return ToadTagUtils.createEntityType(ToadLib.MODID, name);
         }
     }
-}
