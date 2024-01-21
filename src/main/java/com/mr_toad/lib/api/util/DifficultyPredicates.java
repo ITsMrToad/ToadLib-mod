0b1c6de6d8414d20293515e9f204b413e28f819d
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-01-21 15:58:45 +0500

    Create DifficultyPredicates || 1.0.6 update

---
+++ src/main/java/com/mr_toad/lib/api/util/DifficultyPredicates.java
@@ -0,0 +1,21 @@
+package com.mr_toad.lib.api.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.level.Level;
+
+import java.util.function.Predicate;
+
+public class DifficultyPredicates {
+
+    public static final Predicate<Difficulty> HARD_PREDICATE = (difficulty) -> difficulty == Difficulty.HARD;
+
+    public static boolean isHard(Level lvl) {
+        return HARD_PREDICATE.test(lvl.getDifficulty());
+    }
+
+    public static boolean isHarderThanNormal(Level lvl, BlockPos pos) {
+        return lvl.getCurrentDifficultyAt(pos).isHarderThan((float)Difficulty.NORMAL.ordinal());
+    }
+
+}
