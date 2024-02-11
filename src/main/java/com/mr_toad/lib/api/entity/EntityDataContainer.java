ac9366e40ce5312825eb0f8210e35803964e49da
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-02-11 11:15:59 +0500

    Move EntityDataContainer || 1.0.7 upd

--- src/main/java/com/mr_toad/lib/api/entity/EntityDataContainer.java
+++
@@ -1,8 +0,0 @@
-package com.mr_toad.lib.api.entity;
-
-import net.minecraft.network.syncher.SynchedEntityData;
-import org.jetbrains.annotations.NotNull;
-
-public interface EntityDataContainer {
-    @NotNull SynchedEntityData getData();
-}
