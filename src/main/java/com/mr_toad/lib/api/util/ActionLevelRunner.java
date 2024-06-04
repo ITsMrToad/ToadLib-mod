ed9ac89165bc19b7eea3c66801dbe0a9a52fc096
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-06-04 20:26:49 +0500

    Fix level closing after action

--- src/main/java/com/mr_toad/lib/api/util/ActionLevelRunner.java
+++ src/main/java/com/mr_toad/lib/api/util/ActionLevelRunner.java
@@ -1,20 +1,14 @@
 package com.mr_toad.lib.api.util;
 
-import com.mr_toad.lib.core.ToadLib;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
-import java.io.IOException;
 import java.util.function.Consumer;
 
 public record ActionLevelRunner<T extends Level>(T lvl, Consumer<T> action) {
-
-    private static final Marker MARKER = MarkerFactory.getMarker("LevelRunnerError");
 
     public static<T extends Level> void lTry(T lvl, Consumer<T> action) {
         new ActionLevelRunner<>(lvl, action).levelAction();
@@ -30,21 +24,13 @@ public record ActionLevelRunner<T extends Level>(T lvl, Consumer<T> action) {
     }
 
     private void serverLevelAction() {
-        try(T level = this.lvl()) {
-            if (!level.isClientSide()) {
-                this.action().accept(level);
-            }
-        } catch (IOException e) {
-            ToadLib.LOGGER.error(MARKER, "Cannot run server level action: {}", this.action(), e);
-        }
+        if (!level.isClientSide()) {
+            this.action().accept(level); 
+        }   
     }
 
     private void levelAction() {
-        try(T level = this.lvl()) {
-            this.action().accept(level);
-        } catch (IOException e) {
-            ToadLib.LOGGER.error(MARKER, "Cannot run level action: {}", this.action(), e);
-        }
+        this.action().accept(level);
     }
 
 }
