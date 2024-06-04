ed18be3ea275625b4687e04964f6caf259e3756f
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-06-04 11:59:53 +0500

    delete old lib(i too lazy for replace/ update all files) | 1.1.0 upd

--- src/main/java/com/mr_toad/lib/api/entity/CapeableMob.java
+++
@@ -1,5 +0,0 @@
-package com.mr_toad.lib.api;
-
-public interface CapeableMob {
-    void capeTick();
-}
--- src/main/java/com/mr_toad/lib/api/entity/type/records/Add2HorseTypesRecord.java
+++
@@ -1,5 +0,0 @@
-package com.mr_toad.lib.api.entity.type.records;
-
-import net.minecraft.resources.ResourceLocation;
-
-public record Add2HorseTypesRecord(int id, ResourceLocation name, ResourceLocation textureLocation) {}
