f47a1e5a421be8760421f36f00d9891cfa2fd7c0
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2025-01-07 22:54:30 +0500

    Update MtJava | 1.2.0

--- src/main/java/com/mr_toad/lib/mtjava/util/MtJava.java
+++ src/main/java/com/mr_toad/lib/mtjava/util/MtJava.java
@@ -1,9 +1,22 @@
 package com.mr_toad.lib.mtjava.util;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.util.Pair;
+import com.mr_toad.lib.mtjava.util.ImmutablePair;
+import com.mr_toad.lib.mtjava.util.ImmutableTuple;
+import net.minecraft.util.FastColor;
+import net.minecraft.util.Tuple;
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MtJava {
 
+    public static final Pattern HEXADECIMAL = Pattern.compile("\\p{XDigit}+");
+    public static final DateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ss:SS");
     public static final Runnable NOTHING = () -> {};
 
     public static<F, S, P extends Pair<F, S>> ImmutablePair<F, S> toImmutablePair(P pair) {
@@ -12,5 +25,32 @@ public class MtJava {
 
     public static<A, B, T extends Tuple<A, B>> ImmutableTuple<A, B> toImmutableTuple(T tuple) {
         return new ImmutableTuple<>(tuple.getA(), tuple.getB());
+    }
+
+    public static void validateDegrees(float degrees) {
+        if (degrees > 180 || degrees < -180) {
+            throw new IllegalArgumentException("Illegal degree value: '" + degrees + "'");
+        }
+    }
+
+    public static<K, V>ImmutableMap<K, V> inverseMap(Map<V, K> source) {
+        ImmutableMap.Builder<K, V> builder = ImmutableMap.builder();
+        source.forEach((v, k) -> builder.put(k, v));
+        return builder.build();
+    }
+
+    public static String timestamp(long millis) {
+        return TIMESTAMP_FORMAT.format(millis);
+    }
+    
+    public static int rgb2Argb(int input) {
+        int r = (input >> 16) & 255;
+        int g = (input >> 8) & 255;
+        int b = input & 255;
+        return FastColor.ARGB32.color(255, r, g, b);
+    }
+
+    public static String rgb2Hex(int color, boolean hasAlpha) {
+        return "#" + StringUtils.leftPad(Integer.toHexString(color), hasAlpha ? 8 : 6, '0');
     }
 }
