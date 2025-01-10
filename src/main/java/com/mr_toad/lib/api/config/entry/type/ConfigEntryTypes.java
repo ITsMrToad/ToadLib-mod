ae80df69b3b7842881dcb9754370f117784578b6
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2025-01-10 20:16:03 +0500

    New types + pages | 1.2.2

--- src/main/java/com/mr_toad/lib/api/config/entry/type/ConfigEntryTypes.java
+++ src/main/java/com/mr_toad/lib/api/config/entry/type/ConfigEntryTypes.java
@@ -13,8 +13,10 @@ public class ConfigEntryTypes {
     public static final ConfigEntryType BOOL = new ConfigEntryType("bool");
     public static final ConfigEntryType BIG_DECIMAL = new ConfigEntryType("double_float");
     public static final ConfigEntryType DEGREE = new ConfigEntryType("degree");
+    public static final ConfigEntryType PERCENT = new ConfigEntryType("percent");
     public static final ConfigEntryType STRING = new ConfigEntryType("string");
     public static final ConfigEntryType COLOR = new ConfigEntryType("color");
     public static final ConfigEntryType ENUM = new ConfigEntryType("enum");
+    public static final ConfigEntryType PAGE = new ConfigEntryType("page");
 
 }
