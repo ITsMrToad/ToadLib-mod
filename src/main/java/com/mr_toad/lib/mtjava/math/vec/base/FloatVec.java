ff37a424c85686838e37d8349b396753f7faf158
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2025-08-11 18:23:05 +0500

    Vec#f improvements | 1.3.5

--- src/main/java/com/mr_toad/lib/mtjava/math/vec/base/FloatVec.java
+++ src/main/java/com/mr_toad/lib/mtjava/math/vec/base/FloatVec.java
@@ -1,10 +1,36 @@
 package com.mr_toad.lib.mtjava.math.vec.base;
 
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.mr_toad.lib.mtjava.nio.MTNIO;
 import it.unimi.dsi.fastutil.floats.FloatList;
+import it.unimi.dsi.fastutil.floats.FloatPredicate;
+import it.unimi.dsi.fastutil.floats.FloatUnaryOperator;
+import net.minecraft.nbt.FloatTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.network.FriendlyByteBuf;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public interface FloatVec<S> {
+
+    static<S extends FloatVec<S>> S load(Supplier<S> empty, ListTag list) {
+        S vec = empty.get();
+        List<FloatTag> tags = list.stream().map(FloatTag.class::cast).toList();
+        for (int i = 0; i < tags.size(); i++) {
+            vec.set(i, tags.get(i).getAsFloat());
+        }
+        return vec;
+    }
+
+    static<S extends FloatVec<S>> S read(Supplier<S> empty, FriendlyByteBuf buf) {
+        S vec = empty.get();
+        FloatList floats = MTNIO.readFloatList(buf);
+        for (int i = 0; i < floats.size(); i++) {
+            vec.set(i, floats.getFloat(i));
+        }
+        return vec;
+    }
 
     @CanIgnoreReturnValue S set(S other);
 
@@ -58,25 +84,58 @@ public interface FloatVec<S> {
         return this.scale(-1.0F);
     }
 
-    default void normalize() {
-        float f = this.length();
-        if (f < 1.0E-4F) {
-            this.destroy();
-        } else {
-            for (int i = 0; i < this.values().size(); i++) {
-                float v = this.get(i);
-                this.set(i, v / f);
-            }
+    @CanIgnoreReturnValue default S destroy() {
+        return this.set(this.zero());
+    }
+
+    @CanIgnoreReturnValue default S inverse() {
+        return this.scale(-1.0F);
+    }
+
+    @CanIgnoreReturnValue default S def(float scalar) {
+        return this.scale(1.0F / scalar);
+    }
+
+    @CanIgnoreReturnValue default S normalize() {
+        return this.def(this.length());
+    }
+
+    @SuppressWarnings("deprecation")
+    default void setIf(FloatPredicate predicate, FloatUnaryOperator v) {
+        for (int i = 0; i < this.values().doubleStream().filter(predicate::test).toArray().length; i++) {
+            this.set(i, v.apply(this.get(i)));
         }
     }
 
-    default void write(FriendlyByteBuf buf) {
-        this.values().forEach(buf::writeDouble);
+    @SuppressWarnings("deprecation")
+    default void setIf(FloatPredicate predicate, float v) {
+        for (int i = 0; i < this.values().doubleStream().filter(predicate::test).toArray().length; i++) {
+            this.set(i, v);
+        }
     }
 
-    default void read(FriendlyByteBuf buf) {
-        this.values().clear();
-        this.values().addAll(buf.readList(FriendlyByteBuf::readFloat));
+    default boolean anyEqual(float v) {
+        return this.values().doubleStream().anyMatch(d -> d == v);
+    }
+
+    default boolean allEqual(float v) {
+        return this.values().doubleStream().allMatch(d -> d == v);
+    }
+
+    default boolean noneEqual(float v) {
+        return this.values().doubleStream().noneMatch(d -> d == v);
+    }
+
+    default ListTag save() {
+        ListTag list = new ListTag();
+        for (float value : this.values()) {
+            list.add(FloatTag.valueOf(value));
+        }
+        return list;
+    }
+
+    default void write(FriendlyByteBuf buf) {
+        buf.writeCollection(this.values(), FriendlyByteBuf::writeFloat);
     }
 
     @SuppressWarnings("deprecation")
@@ -84,3 +143,4 @@ public interface FloatVec<S> {
         return "Vec" + this.size() + "[" + String.join(",", this.values().stream().map(String::valueOf).toList()) + "]";
     }
 }
+
